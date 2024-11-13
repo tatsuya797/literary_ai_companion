71b017af75c4c8a8e7281ae370729316b692eb99
tatsuya797 <tsy16spxp797@gmail.com>
2024-11-13 16:59:49 +0900

    Update aozora_preprocess.py

--- aozora_preprocess.py
+++ aozora_preprocess.py
@@ -22,8 +22,9 @@ def text_cleanse_df(df):
     atx = list(df[df['text'].str.contains('底本：')].index)
     if head_tx == []:
         # もし'---…'区切りが無い場合は、作家名の直後に本文が始まる前提
-        head_tx = list(df[df['text'].str.contains(author_name)].index)
-        head_tx_num = head_tx[0] - 1
+          head_tx = list(df[df['text'].str.contains(author_name)].index)
+          head_tx_num = head_tx[0] - 1
+          df_e = df[head_tx_num:atx[0]]
     else:
         # 2個目の'---…'区切り直後から本文が始まる
           additional_intro = df[max(0,head_tx[0]) -1:head_tx[0]]
