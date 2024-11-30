ee08c95e0309de5037ca8830119a04bc9bb916a3
tatsuya797 <tsy16spxp797@gmail.com>
2024-11-30 20:16:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if selected_bot == "芥川龍之介":
         selected_title = st.selectbox("対話したい作品を選んでください:", titles, key="title_selectbox")
         if st.button("会話を始める", key="start_conversation"):
             # ページ遷移
-            url = "https://literaryaicompanion-prg5zuxubou7vm6rxpqujs.streamlit.app/akutagawa_bot"
+            url = f"https://literaryaicompanion-prg5zuxubou7vm6rxpqujs.streamlit.app/akutagawa_bot?title={selected_title}"
             st.markdown(f'<meta http-equiv="refresh" content="0; url={url}">', unsafe_allow_html=True)
     else:
         st.write("作品リストを取得できませんでした。データベースを確認してください。")
