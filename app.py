2232ec1648fcf8bc6157fead5cafdbb411c9b103
tatsuya797 <tsy16spxp797@gmail.com>
2024-11-30 19:24:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -107,13 +107,6 @@ if "page" in st.session_state and st.session_state["page"] == "chat":
     st.write("対話画面に移動中...")  # 実際のアプリでは対話ページに移行します
 
 
-    # 芥川ボットの選択に応じてリンクボタンを表示
-if selected_bot == "芥川龍之介":
-    st.write("会話を始めるボタンを押すと bot.py に移動します。")
-    st.markdown(
-        f'<a href="https://literaryaicompanion-prg5zuxubou7vm6rxpqujs.streamlit.app/akutagawa_bot" class="btn-start">会話を始める</a>',
-        unsafe_allow_html=True,
-    )
 
     
 
