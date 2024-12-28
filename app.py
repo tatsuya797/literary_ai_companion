1c412b2b4d6b79450c58939e6016d148c33ef209
tatsuya797 <tsy16spxp797@gmail.com>
2024-12-28 18:48:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,8 @@ if st.session_state["logged_in"]:
             selected_title = st.selectbox("対話したい作品を選んでください:", titles, key="title_selectbox")
             if st.button("会話を始める", key="start_conversation"):
                 # ページ遷移
-                url = f"https://literaryaicompanion-prg5zuxubou7vm6rxpqujs.streamlit.app/akutagawa_bot"
+                url = (
+                f"https://literaryaicompanion-prg5zuxubou7vm6rxpqujs.streamlit.app/akutagawa_bot"
                 f"?id={some_id}"
                 f"&username={username}"
                 f"&author={selected_bot}"
